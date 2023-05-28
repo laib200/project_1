94f7bdb9f01445cf834553fe861f0f734a793174
laib.200 <laib.dz.200@gmail.com>
2023-05-28 15:07:14 +0100

    full config v2.0.0

--- server/api/views.py
+++ server/api/views.py
@@ -1,12 +1,30 @@
+from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
 from app.serializers import MealsSerializer,RatingSerializer
 from app.models import Meals,Rating
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
 
 
 
 class ViewMeals(ModelViewSet):
     queryset = Meals.objects.all()
     serializer_class = MealsSerializer
+    
+    @action(detail=False, methods=['post'])
+    def rate_meals(self, request,pk=None,):
+        if 'stars' in request.data:
+            meal = Meals.objects.get(id=pk)
+            username =  request.get('username')
+            user = User.objects.filter(username=username)
+            stars = request.get('stars')
+            try:
+                pass
+            except:
+                pass
+        else:
+            pass
 
 class ViewRating(ModelViewSet):
     queryset = Rating.objects.all()
