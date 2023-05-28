f83f649fdef67b7de1c9eaf85fb0b1d72f2d9249
laib.200 <laib.dz.200@gmail.com>
2023-05-28 11:52:37 +0100

    full config v2.0.0

--- server/api/views.py
+++ server/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
-from app.serializers import Meals,Rating
+from app.serializers import MealsSerializer,RatingSerializer
 from app.models import Meals,Rating
 
 
 
 class ViewMeals(ModelViewSet):
     queryset = Meals.objects.all()
-    serializer_class = Meals
+    serializer_class = MealsSerializer
 
 class ViewRating(ModelViewSet):
     queryset = Rating.objects.all()
-    serializer_class = Rating
+    serializer_class = RatingSerializer
--- server/core/urls.py
+++ server/core/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('',include("urls.urls")),
 ]
