33813ec438adac89b5410e9f692efa3646f8438b
laib.200 <laib.dz.200@gmail.com>
2023-05-29 10:58:26 +0100

    V2.3.0

--- server/api/views.py
+++ server/api/views.py
@@ -1,41 +1,47 @@
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
-from app.serializers import MealsSerializer,RatingSerializer
-from app.models import Meals,Rating
+from app.serializers import MealsSerializer, RatingSerializer
+from app.models import Meals, Rating
 from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.response import Response
 
 
-
 class ViewMeals(ModelViewSet):
     queryset = Meals.objects.all()
     serializer_class = MealsSerializer
-    
-    @action(detail=False, methods=['post'])
-    def rate_meals(self, request,pk=None,):
-        if 'stars' in request.data:
+
+    @action(detail=True, methods=["post"])
+    def rate_meals(
+        self,
+        request,
+        pk=None,
+    ):
+        if "stars" in request.data:
             meal = Meals.objects.get(id=pk)
-            username =  request.get('username')
+
+            username = request.data["username"]
+
             user = User.objects.get(username=username)
-            stars = request.get('stars')
+
+            stars = request.data["stars"]
+
             try:
                 rating = Rating.objects.get(user=user.id, meal=meal.id)
                 rating.stars = stars
                 rating.save()
-                serializer = RatingSerializer(rating=rating,many=False)
-                response = {
-                    'message': 'Rating updated',
-                    'data': serializer.data 
-                }
-                return Response(response,status=status.HTTP_200_OK)
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "Rating updated", "data": serializer.data}
+                return Response(response, status=status.HTTP_301_MOVED_PERMANENTLY)
             except:
-                rating = Rating.objects.create(meal=meal,user=user,stars=stars)
+                rating = Rating.objects.create(meal=meal, user=user, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {"massage": "Rating created", "data": serializer.data}
+                return Response(response, status=status.HTTP_201_CREATED)
         else:
-            response = {
-                "message": 'bad request'
-            }
-            return Response(response,status=status.HTTP_400_BAD_REQUEST)
+            response = {"message": "bad request"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ViewRating(ModelViewSet):
     queryset = Rating.objects.all()
